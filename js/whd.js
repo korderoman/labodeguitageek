70c589132eaa5ad796d81e3b873b261bf2103128
Henry <elpadredelcordero@gmail.com>
2017-11-24 19:18:50 -0500

    Versión 2 de web
    Se mejora la transición de páginas, se implementó la llamada de páginas anidadas en las llamadas de ID

--- js/whd.js
+++ js/whd.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $("#paginas").load("./html/p1.html");
+    var test3=document.getElementById("p1_1");
     $("a#nosotros").click(function (e) { 
        e.preventDefault();
         $("#separador").load("./html/nosotros.html");
@@ -8,13 +8,26 @@ $(document).ready(function(){
         e.preventDefault();
          $("#separador").load("./html/contacto.html");
      });
-    $("#paginador").bootpag({total:5,page:1}).on("page",function(event,num){
-        $("#paginas").fadeOut(500).hide().load("./html/p"+num+".html").fadeIn(500);
-        //.fadeOut(1000).hide().slideUp(300).slideDown(300).show().fadeIn(1000);
-        $(this).bootpag({total:5,maxVisible:5});
      
-    });
-    
    
+
+
+   /******Paginador */
+$("#paginador").bootpag({total:5,page:1}).on("page",function(event,num){
+    $("#paginas").fadeOut(500).hide().load("./html/p"+num+".html").fadeIn(500);
+    $(this).bootpag({total:5,maxVisible:5}).fadeOut(500).hide().fadeIn(500);
+  /**Fin de Paginador**/   
+});
+
+$(document).on("click","#p1_1", function(e){
+    /*la funcion click funciona cuando los elementos del DOM han sido registrados, esto quiere decir
+    que como las demás hojas del paginador están a la espera de una llamada en función del paginador
+    no es posible que click los llame pues no existen en el registro del DOM, para ello usamos la funcion
+    on, que va funcionar cada vez que exista un rastos del id en el dom*/
+    e.preventDefault();
+    $("#separador").load("./html/productos/p1_1.html");
+    console.log(test3);
+});
+
 
 });
